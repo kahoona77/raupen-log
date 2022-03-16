2ada45acf59b499d6c2963226baa7ee8557e63f5
Benjamin <benjamin@kuoo.de>
2022-03-16 12:04:20 +0100

    add login & security

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\User;
 use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
